824f8ac0ac075180b9b3f462430c9c4fb63ee4ae
Alberto Madueno <alberto_mn@outlook.com>
2022-05-14 13:37:08 -0500

    Initial Commit

---
+++ GeometryReaderPractice/GeometryReaderPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometryReaderPracticeApp.swift
+//  GeometryReaderPractice
+//
+//  Created by Alberto Madueño Navarrete on 5/14/22.
+//
+
+import SwiftUI
+
+@main
+struct GeometryReaderPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
